3ac8b91104ed74c47fe61ecf36380230c396f647
Andrea Bonatti <andreabonatti92@gmail.com>
2021-12-02 09:07:54 +0100

    Room implementation + repository

--- app/src/main/java/com/example/mvvmcrudtutorial/db/Subscriber.kt
+++ app/src/main/java/com/example/mvvmcrudtutorial/db/Subscriber.kt
@@ -1,11 +1,22 @@
 package com.example.mvvmcrudtutorial.db
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Room di default usa i nomi della classe se voglio cambiarli allora uso
+// tableName per le Entity e name per le ColumnInfo
 
 @Entity(tableName = "subscriber_data_table")
 data class Subscriber(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
     val id: Int,
+
+    @ColumnInfo(name = "subscriber_name")
     val name: String,
+
+    @ColumnInfo(name = "subscriber_email")
     val email: String
-) {
-}
+)
